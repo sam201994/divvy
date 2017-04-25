aa215b1e00cc70dedd31231f8793281d951d7877
Smriti <smriti@Smritis-MacBook-Pro.local>
2017-04-25 18:10:43 +0530

    fixed signin and sinup error

--- client/src/components/Auth/SignIn.jsx
+++ client/src/components/Auth/SignIn.jsx
@@ -7,25 +7,34 @@ import store from '../../redux/store';
 import { connect } from 'react-redux';
 
 /* other files */
-import { handleSignin } from '../../redux/actions/authActions.js';
+import { handleSignin, setError } from '../../redux/actions/authActions.js';
 
-const Signin = ({ auth }) => {
-  console.log("inside sing in component")
-  return (
-    <div id="signin">
-      <h2>Log in to Begin Divvy</h2>
-      <form name="signinForm" onSubmit={handleSignin}>
-        <div>
-          <input type="text" name="username" placeholder="username" required />
-        </div>
-        <div>
-          <input type="password" name="password" placeholder="Password" required />
-        </div>
-        <button type="submit" className="btn">Signin</button>
-      </form>
-      <div className="error-text">{auth.authError}</div>
-  </div>
-  )
+class Signin extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount(){
+    setError();
+  }
+  render() {
+    const { auth } = this.props;
+    return (
+      <div id="signin">
+        <h2>Log in to Begin Divvy</h2>
+        <form name="signinForm" onSubmit={handleSignin}>
+          <div>
+            <input type="text" name="username" placeholder="username" required />
+          </div>
+          <div>
+            <input type="password" name="password" placeholder="Password" required />
+          </div>
+          <button type="submit" className="btn">Signin</button>
+        </form>
+        <div className="error-text">{auth.authError}</div>
+      </div>
+    )
+  }
 }
 
 export default connect((store) => {
@@ -33,3 +42,5 @@ export default connect((store) => {
     auth: store.auth
   };
 })(Signin);
+
+
--- client/src/components/Auth/SignUp.jsx
+++ client/src/components/Auth/SignUp.jsx
@@ -7,29 +7,42 @@ import store from '../../redux/store';
 import { connect } from 'react-redux';
 
 /* other files */
-import { handleSignup } from '../../redux/actions/authActions.js';
+import { handleSignup, setError } from '../../redux/actions/authActions.js';
 
-const Signup = ({ auth }) => (
-  <div id="signup">
-    <h2>Manage your expenses</h2>
-    <form name="signupForm" onSubmit={handleSignup}>
-      <div>
-        <input type="text" name="name" placeholder="name" required />
+class Signup extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount(){
+    setError();
+  }
+
+  render() {
+    const { auth } = this.props;
+    return (
+      <div id="signup">
+        <h2>Manage your expenses</h2>
+        <form name="signupForm" onSubmit={handleSignup}>
+          <div>
+            <input type="text" name="name" placeholder="name" required />
+          </div>
+          <div>
+           <input type="text" name="username" placeholder="username" required />
+          </div>
+          <div>
+            <input type="password" name="password" placeholder="Password" required />
+          </div>
+          <div>
+           <input type="password" name="confirm_password" placeholder="Confirm Password" required />
+          </div>
+          <button type="submit" className="btn">Signup</button>
+        </form>
+        <div className="error-text">{auth.authError}</div>
       </div>
-      <div>
-        <input type="text" name="username" placeholder="username" required />
-      </div>
-      <div>
-        <input type="password" name="password" placeholder="Password" required />
-      </div>
-      <div>
-        <input type="password" name="confirm_password" placeholder="Confirm Password" required />
-      </div>
-      <button type="submit" className="btn">Signup</button>
-    </form>
-    <div className="error-text">{auth.authError}</div>
-  </div>
-);
+    )
+  }
+}
 
 export default connect((store) => {
   return {
